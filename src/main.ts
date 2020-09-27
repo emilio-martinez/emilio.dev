60e2432bd1e160594b71594eadef17dfa9efa94f
Emilio Martinez <e.marticor@gmail.com>
2020-09-26 20:22:57 -0700

    build: remedy missing built files on first serve run

--- src/main.ts
+++
@@ -1,4 +0,0 @@
-enum Bla {
-  ONE,
-  TWO,
-}
--- src/site/_data/bundlerAssets.js
+++ src/site/_data/bundlerAssets.js
@@ -1,14 +1,19 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const { resolve } = require('path');
-const { readJson } = require('fs-extra');
+const { pathExists, readJson } = require('fs-extra');
 
 const MAIN_ASSET_MANIFEST_PATH = resolve(__dirname, '../assets/build/main-manifest.json');
 const ADMIN_ASSET_MANIFEST_PATH = resolve(__dirname, '../assets/build/admin-manifest.json');
 
+async function readJsonIfExists(filePath) {
+  const exists = await pathExists(filePath);
+  return exists ? await readJson(filePath) : null;
+}
+
 module.exports = async function () {
   return {
-    main: await readJson(MAIN_ASSET_MANIFEST_PATH),
-    admin: await readJson(ADMIN_ASSET_MANIFEST_PATH),
+    main: await readJsonIfExists(MAIN_ASSET_MANIFEST_PATH),
+    admin: await readJsonIfExists(ADMIN_ASSET_MANIFEST_PATH),
   };
 };
