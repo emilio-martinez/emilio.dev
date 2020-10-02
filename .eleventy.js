30e56e84cd48f45a8a5179713c0a4f6a4793cd37
Emilio Martinez <e.marticor@gmail.com>
2020-10-02 01:26:35 -0700

    refactor: minify site html output

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const { resolve } = require('path');
+const htmlmin = require('html-minifier');
 
 module.exports = function (config) {
   config.addPassthroughCopy(`src/site/robots.txt`);
@@ -6,6 +7,21 @@ module.exports = function (config) {
 
   // Add bundler built assets as watch targets. They're ignored by default due to gitignore.
   config.addWatchTarget(resolve(__dirname, `./src/site/assets/build/`));
+
+  // Minify HTML
+  config.addTransform('htmlmin', function (content, outputPath) {
+    if (outputPath.endsWith('.html')) {
+      return htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+      });
+    }
+
+    return content;
+  });
 
   return {
     dir: {
